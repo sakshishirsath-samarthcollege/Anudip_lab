f32386e0817c940a3554f5ce0dcec62fa14295ab
sakshi-17 <151017069+sakshishirsath-samarthcollege@users.noreply.github.com>
2023-11-16 15:22:39 +0530

    Update ROI.java

--- ROI.java
+++ ROI.java
@@ -4,7 +4,35 @@ public class ROI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		int age;
+		char gender;
+		double roi;
+		System.out.println("Enter Your Age=");
+		age=sc.nextInt();
+		System.out.println("Enter Your Gender(M / f)=");
+		gender=sc.next().charAt(0);
+		if(age>=60)
+		{
+			if(gender == 'M')
+			{
+				roi=7.0;
+			}
+			else if(gender == 'F')
+			{
+				roi=7.5;
+			}
+			else
+			{
+				System.out.println("Invalid Gender");
+				return;
+			}
+		}
+		else
+		{
+			roi=5.0;
+		}
+		System.out.println("Your Rate Of Interest  is :"+roi+"%");
 	}
 
 }
